b58fcf783bcf4b575a2574e28700b5b217346efe
Luz Moreno Campos <luzdirection@gmail.com>
2026-03-24 12:10:23 -0500

    created classes and method

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -15,3 +15,53 @@ const date = select('.date');
 const postText = select('.posted-text');
 const postedImage = select('.post-image')
 
+class User {
+  #id; 
+  #name; 
+  #userName; 
+  #email;
+  constructor(id, name, userName, email) {
+    this.#id = id;
+    this.#name = name;
+    this.#userName = userName;
+    this.#email = email;
+  }
+
+  get id() {return this.#id}
+  get name() {return this.#name}
+  get userName() {return this.#userName}
+  get email() {return this.#email}
+
+  getInfo() {
+    userName.innerText = this.#userName;
+    name.innerText = this.#name;
+    id.innerText = this.#id;
+    email.innerText = this.#email;
+
+  }
+}
+
+class Subscriber extends User {
+  #pages;
+  #groups;
+  #canMonetize;
+
+  constructor(id, name, userName, email, pages, groups, canMonetize) {
+    super(id, name, userName, email)
+    this.#pages = pages;
+    this.#groups = groups;
+    this.#canMonetize = canMonetize;
+  }
+get pages() {return this.#pages}
+get groups() {return this.#groups}
+get canMonetize() {return this.#canMonetize}
+
+ getInfo() {
+    super.getInfo();
+    pages.innerText = this.#pages;
+    groups.innerText = this.#groups;
+    canMonetize.innerText = this.#canMonetize ? "Yes" : "No";
+   
+  }
+
+}
